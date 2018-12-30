995a5c0cc74e1cbb05a4eead5bc8f9f36611a5f2
xujin <Software_King@qq.com>
2018-12-30 14:22:10 +0800

    halo-admin初始化

---
+++ spring-cloud-admin-web/src/main/java/cn/springcloud/admin/SpringCloudAdminWebApplication.java
@@ -0,0 +1,18 @@
+package cn.springcloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringCloudAdminWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudAdminWebApplication.class, args);
+	}
+}
